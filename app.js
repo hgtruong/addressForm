e289d4e1db8984591e4ca736185d04388b0a3ab6
hgtruong <huygtruong@gmail.com>
2019-03-31 17:52:36 -0700

    - Only allow edit if form is empty - Added dummy data

--- app.js
+++ app.js
@@ -1,139 +1,136 @@
-let addressesStorage = [];
-
 $(document).ready(function() {
+  console.log('data is', dummyData);
+  let addressesStorage = dummyData;
+  console.log('Document is ready.');
+  renderAddresses();
 
-
-
-    console.log('Document is ready.');
-    // console.log('Storage before', window.localStorage);
-    renderAddresses();
-
-    // Form Validation
-    $('.ui.form').form({
-      inline: true,
-      fields: {
-        addressee: {
-          identifier: 'addressee',
-          rules: [
-            {
-              type   : 'empty',
-              prompt : 'Please enter first and last name'
-            },
-            {
-              type   : 'maxLength[30]',
-              prompt : 'Only 30 characters are allowed'
-            }
-          ]
-        },
-        attention: {
-          identifier: 'attention',
-          rules: [
-            {
-              type   : 'maxLength[30]',
-              prompt : 'Only 30 characters are allowed'
-            }
-          ]
-        },
-        addressOne: {
-          // TODO: Use API to validate the address
-          identifier: 'addressOne',
-          rules: [
-            {
-              type   : 'empty',
-              prompt : 'Please enter a valid street number and name'
-            },
-            {
-              type   : 'maxLength[50]',
-              prompt : 'Only 50 characters are allowed'
-            }
-          ]
-        },
-        addressTwo: {
-          identifier: 'addressTwo',
-          rules: [
-            {
-              type   : 'maxLength[50]',
-              prompt : 'Only 50 characters are allowed'
-            }
-          ]
-        },
-        city: {
-          identifier: 'city',
-          rules: [
-            {
-              type   : 'empty',
-              prompt : 'Please enter a valid city'
-            },
-            {
-              type   : 'maxLength[30]',
-              prompt : 'Only 30 characters are allowed'
-            }
-          ]
-        },
-        state: {
-          identifier: 'state',
-          rules: [
-            {
-              type   : 'empty',
-              prompt : 'Please select a state'
-            }
-          ]
-        },
-        phoneNumber: {
-          identifier: 'phoneNumber',
-          rules: [
-            {
-              type   : 'regExp',
-              value  : '^[0-9]{3}-[0-9]{3}-[0-9]{4}$',
-              prompt : 'Please use 123-456-7899 format'
-            }
-          ]
-        },
+  // Form Validation
+  $('.ui.form').form({
+    inline: true,
+    fields: {
+      addressee: {
+        identifier: 'addressee',
+        rules: [
+          {
+            type   : 'empty',
+            prompt : 'Please enter first and last name'
+          },
+          {
+            type   : 'maxLength[30]',
+            prompt : 'Only 30 characters are allowed'
+          }
+        ]
       },
-      onSuccess: function (event) {
-        event.preventDefault();
+      attention: {
+        identifier: 'attention',
+        rules: [
+          {
+            type   : 'maxLength[30]',
+            prompt : 'Only 30 characters are allowed'
+          }
+        ]
+      },
+      addressOne: {
+        // TODO: Use API to validate the address
+        identifier: 'addressOne',
+        rules: [
+          {
+            type   : 'empty',
+            prompt : 'Please enter a valid street number and name'
+          },
+          {
+            type   : 'maxLength[50]',
+            prompt : 'Only 50 characters are allowed'
+          }
+        ]
+      },
+      addressTwo: {
+        identifier: 'addressTwo',
+        rules: [
+          {
+            type   : 'maxLength[50]',
+            prompt : 'Only 50 characters are allowed'
+          }
+        ]
+      },
+      city: {
+        identifier: 'city',
+        rules: [
+          {
+            type   : 'empty',
+            prompt : 'Please enter a valid city'
+          },
+          {
+            type   : 'maxLength[30]',
+            prompt : 'Only 30 characters are allowed'
+          }
+        ]
+      },
+      state: {
+        identifier: 'state',
+        rules: [
+          {
+            type   : 'empty',
+            prompt : 'Please select a state'
+          }
+        ]
+      },
+      phoneNumber: {
+        identifier: 'phoneNumber',
+        rules: [
+          {
+            type   : 'regExp',
+            value  : '^[0-9]{3}-[0-9]{3}-[0-9]{4}$',
+            prompt : 'Please use 123-456-7899 format'
+          }
+        ]
+      },
+    },
+    onSuccess: function (event) {
+      event.preventDefault();
 
-        // Reset button value to "Submit" if changed
-        $('#submitBtn').html('Submit');
+      // Reset button value to "Submit" if changed
+      $('#submitBtn').html('Submit');
 
-        let newAddress = {};
-        newAddress['addressee'] = getFieldValue('addressee');
-        newAddress['attention'] = getFieldValue('attention');
-        newAddress['residental'] = getCheckboxValue('residental');
-        newAddress['addressOne'] = getFieldValue('addressOne');
-        newAddress['addressTwo'] = getFieldValue('addressTwo');
-        newAddress['city'] = getFieldValue('city');
-        newAddress['state'] = getFieldValue('state');
-        newAddress['phoneNumber'] = getFieldValue('phoneNumber');
-        // console.log('newAddress Object', newAddress);
+      let newAddress = {};
+      newAddress['addressee'] = getFieldValue('addressee');
+      newAddress['attention'] = getFieldValue('attention');
+      newAddress['residental'] = getCheckboxValue('residental');
+      newAddress['addressOne'] = getFieldValue('addressOne');
+      newAddress['addressTwo'] = getFieldValue('addressTwo');
+      newAddress['city'] = getFieldValue('city');
+      newAddress['state'] = getFieldValue('state');
+      newAddress['phoneNumber'] = getFieldValue('phoneNumber');
 
-        // addToStorage(newAddress);
-        addressesStorage.push(newAddress);
-        renderAddresses();
-        // Reset all fields when successful
-        $(".ui.form")[0].reset();
-      }
-    });
-
-    // function addToStorage (newAddress) {
-    //   // localStorage.clear();
-    //   var numOfAddressInStorage = localStorage.length;
-    //   localStorage.setItem(`address-${numOfAddressInStorage+1}`, JSON.stringify(newAddress));
-    // }
-
-    function getCheckboxValue(fieldId) {
-      var checkbox = $('.ui.form').find(`input#${fieldId}`);
-      return checkbox[0].checked ? 'Yes' : "No";
+      console.log("newAddress", newAddress);
+      addressesStorage.unshift(newAddress);
+      renderAddresses();
+      // Reset all fields when successful
+      $(".ui.form")[0].reset();
     }
+  });
 
-    function getFieldValue(fieldId) { 
-      // 'get field' is part of Semantics form behavior API
-      return $('.ui.form').form('get field', fieldId).val();
-    }
+  function getCheckboxValue(fieldId) {
+    var checkbox = $('.ui.form').find(`input#${fieldId}`);
+    return checkbox[0].checked ? 'Yes' : "No";
+  }
 
-    // Edit double clicked table row
-    $(document).on('dblclick', '#addressesBody tr', function(event) {
-      console.log('You clicked row '+ ($(this).index()));
+  function getFieldValue(fieldId) { 
+    // 'get field' is part of Semantics form behavior API
+    return $('.ui.form').form('get field', fieldId).val();
+  }
+
+  // Edit double clicked table row
+  $(document).on('dblclick', '#addressesBody tr', function(event) {
+    // console.log('You clicked row '+ ($(this).index()));
+    console.log('addressee is', $("#addressee").val());
+
+    if (($('#addressee').val().length 
+        || $('#addressOne').val().length 
+        || $('#city').val().length 
+        || $('#state').val().length 
+        || $('#phoneNumber').val().length ) === 0) {
+
       // Change from 'submit' to 'update'
       $('#submitBtn').html("Update");
 
@@ -166,49 +163,51 @@ $(document).ready(function() {
       addressesStorage.splice(clickedRowIndex, 1);
       $(this).remove();
       renderAddresses();
-
-    });
-
-    // TODO: Should have warning dialog
-    $('#clearBtn').click(function() {
-
-      $('.coupled.modal')
-      .modal({
-        allowMultiple: false
-      });
-      // open second modal on first modal buttons
-      $('.ui.tiny.modal.two')
-      .modal({
-        onApprove: function() {
-          addressesStorage = [];
-          renderAddresses();
-        }
-      })
-      .modal('attach events', '.ui.tiny.modal.one .ui.red.approve.button')
-      
-      $('.ui.tiny.modal.one')
-      .modal('show');
-
-    });
-
-    function renderAddresses() {
-      $("#addressesTable tbody").html("");
-      console.log('storage', addressesStorage);
-
-      for(var i = 0; i < addressesStorage.length; i++) {
-        var newRowContent = `
-        <tr>
-          <td data-label="Name">${addressesStorage[i].addressee}</td>
-          <td data-label="Attention">${addressesStorage[i].attention}</td>
-          <td data-label="Residental">${addressesStorage[i].residental}</td>
-          <td data-label="AddressOne">${addressesStorage[i].addressOne}</td>
-          <td data-label="AddressTwo">${addressesStorage[i].addressTwo}</td>
-          <td data-label="City">${addressesStorage[i].city}</td>
-          <td data-label="State">${addressesStorage[i].state}</td>
-          <td data-label="PhoneNumber">${addressesStorage[i].phoneNumber}</td>
-        </tr>`
-
-        $("#addressesTable tbody").append(newRowContent);
-      }
     }
+    
+
+  });
+
+  // TODO: Should have warning dialog
+  $('#clearBtn').click(function() {
+
+    $('.coupled.modal')
+    .modal({
+      allowMultiple: false
+    });
+
+    $('.ui.tiny.modal.two')
+    .modal({
+      onApprove: function() {
+        addressesStorage = [];
+        renderAddresses();
+      }
+    })
+    .modal('attach events', '.ui.tiny.modal.one .ui.red.approve.button');
+    
+    $('.ui.tiny.modal.one')
+    .modal('show');
+
+  });
+
+  function renderAddresses() {
+    $("#addressesTable tbody").html("");
+    console.log('storage', addressesStorage);
+
+    for(var i = 0; i < addressesStorage.length; i++) {
+      var newRowContent = `
+      <tr>
+        <td data-label="Name">${addressesStorage[i].addressee}</td>
+        <td data-label="Attention">${addressesStorage[i].attention}</td>
+        <td data-label="Residental">${addressesStorage[i].residental}</td>
+        <td data-label="AddressOne">${addressesStorage[i].addressOne}</td>
+        <td data-label="AddressTwo">${addressesStorage[i].addressTwo}</td>
+        <td data-label="City">${addressesStorage[i].city}</td>
+        <td data-label="State">${addressesStorage[i].state}</td>
+        <td data-label="PhoneNumber">${addressesStorage[i].phoneNumber}</td>
+      </tr>`
+
+      $("#addressesTable tbody").append(newRowContent);
+    }
+  }
 });
---
+++ dummyData.js
@@ -0,0 +1,73 @@
+const dummyData =
+  [
+    {
+      addressOne: "123 North Haste Street",
+      addressTwo: "",
+      addressee: "John Johnson",
+      attention: "",
+      city: "Davis",
+      phoneNumber: "408-798-0034",
+      residental: "Yes",
+      state: "IN"
+    },
+    {
+      addressOne: "2512 Littleton Drive",
+      addressTwo: "",
+      addressee: "Daisy Tattle",
+      attention: "",
+      city: "Modesto",
+      phoneNumber: "408-234-1233",
+      residental: "No",
+      state: "CA"
+    },
+    {
+      addressOne: "2345 Larry Road",
+      addressTwo: "",
+      addressee: "Gary Scott",
+      attention: "",
+      city: "Knoxville",
+      phoneNumber: "408-798-0034",
+      residental: "Yes",
+      state: "TN"
+    },  
+    {
+      addressOne: "10123 Southwest Solar Court",
+      addressTwo: "",
+      addressee: "Henry Siew",
+      attention: "",
+      city: "Denver",
+      phoneNumber: "344-234-3455",
+      residental: "No",
+      state: "CO"
+    },  
+    {
+      addressOne: "345 South Haste Street",
+      addressTwo: "",
+      addressee: "Kathy Long",
+      attention: "",
+      city: "Louisville",
+      phoneNumber: "525-234-2356",
+      residental: "No",
+      state: "NY"
+    },
+    {
+      addressOne: "753 Tully Street",
+      addressTwo: "Apt# 423",
+      addressee: "Mikayla Short",
+      attention: "David Holten",
+      city: "Witcher",
+      phoneNumber: "232-679-7998",
+      residental: "Yes",
+      state: "KS"
+    },
+    {
+      addressOne: "564 Capital Expressway",
+      addressTwo: "Unit 231",
+      addressee: "Andy Wahel",
+      attention: "Ariel Walsh",
+      city: "Tena",
+      phoneNumber: "146-432-9003",
+      residental: "Yes",
+      state: "OK"
+    }
+  ]
